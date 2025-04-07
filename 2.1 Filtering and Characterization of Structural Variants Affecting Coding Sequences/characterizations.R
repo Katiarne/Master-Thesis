4f8db94965443007fc25073222477587331be72a
Katiarne <147985288+Katiarne@users.noreply.github.com>
2025-04-07 09:03:49 +0200

    characterizations.R

--- 2.1 Filtering and Characterization of Structural Variants Affecting Coding Sequences/characterizations.R
+++ 2.1 Filtering and Characterization of Structural Variants Affecting Coding Sequences/characterizations.R
@@ -1,27 +1,30 @@
+# Load intesect file created in bedtools.sh
 intersect_data <- read.table("intersect_overlap_output")
+
+
+# make sure no lncRNA
+cds_only <- intersect_data[grep("ID=CDS", intersect_data$V9), ]
 
 unique(intersect_data$V9) # to get the unique data points
 
-# list of uniqe genes
-attributions <- intersect_data$V9
-unique_genes <- data.frame(table(attributions))
-colnames(unique_genes)[colnames(unique_genes) == "Freq"] <- "SVs"
-unique_genes
-
-# count nr. of bases in Reference sequene and alternative sequence
+# telle antall baser fra "reffrence" og "alternative"
 library(tidyverse)
 
 intersect_data <- intersect_data %>%
   mutate(reference_count = nchar(V13, type = "chars"),
          alternative_count = nchar(V14, type = "chars"))
 
-# if reference have highest nr. of bases -> Deletions
-# if alterantive have highest nr. of bases -> Insertion
+# decide if deletion or insertion
 intersect_data <- intersect_data %>%
   mutate(variant_type = ifelse(reference_count > alternative_count, 
                                "deletion", 
                                "insertion"))
 
-# new tabel with only deletions
+# new datafraim with only deletions
 deletion_data <- intersect_data %>%
   filter(variant_type == "deletion")
+
+length(unique(deletion_data$V9))
+
+
+
